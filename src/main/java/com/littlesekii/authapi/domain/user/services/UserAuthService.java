1b20cd02eae843072af095f220cc8b84a07eb888
littlesekii <littlesekii@gmail.com>
2024-02-19 14:55:58 -0300

    feat: added user authentication service

---
+++ src/main/java/com/littlesekii/authapi/domain/user/services/UserAuthService.java
@@ -0,0 +1,21 @@
+package com.littlesekii.authapi.domain.user.services;
+
+import com.littlesekii.authapi.domain.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserAuthService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public UserAuthService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+}
